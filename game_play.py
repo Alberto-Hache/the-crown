33af60258f362424410e1f561a9a2d9f982fdd1b
Alberto H <Alberto@iMac-de-Alberto.local>
2020-02-08 22:29:07 +0100

    Comment make_pseudo_move()

--- game_play.py
+++ game_play.py
@@ -153,7 +153,9 @@ def make_pseudo_move(board, coord1, coord2):
     """
     Given a *legal* position, try to make a pseudomove.
     Detect if it would be ilegal first.
-    If it's legal, handle special moves: Soldier promotion, Prince crowning.
+    If it's legal, detect some end of game conditions:
+    - Prince crowning
+    - Opponent is left with no pieces
 
     Input
         board: Board, coord1: int, coord2: int
@@ -162,23 +164,6 @@ def make_pseudo_move(board, coord1, coord2):
         game_status: int*
         * (ON_GOING / VICTORY_CROWNING / VICTORY_NO_PIECES_LEFT
         DRAW_NO_PRINCES_LEFT / DRAW_STALEMATE / DRAW_THREE_REPETITIONS)
-
-    Cases managed:
-    a) Detection of ilegal moves, once done, according to is_legal().
-
-    b) legal moves:
-        b.1) Playing Prince reaches the crown.
-            -> new_board, is_legal=True,
-               result=PLAYER_WINS, game_end=True, game_status=VICTORY_CROWNING
-
-        b.2) Capture of last opponent's piece.
-            -> new_board, is_legal=True,
-               result=PLAYER_WINS, game_end=True, game_status=VICTORY_NO_PIECES_LEFT
-
-        b.3) Normal move, normal capture (no last opponent's piece).
-            -> new_board, is_legal=True,
-               result=None, game_end=False, game_status=ON_GOING
-
     """
 
     # Create new board on which to try the move.
